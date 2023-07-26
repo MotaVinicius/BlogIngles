33d2b5447532dff5abdbf9413d98df5cb353373e
Vinicius Mota <motavinicius@outlook.com>
2023-07-25 21:29:56 -0300

    menu resposivo

--- JS/script.js
+++ JS/script.js
@@ -1,3 +1,23 @@
+function menuShow(){
+    let menuMobile = document.querySelector('.menu-mobile');
+    let icone = document.querySelector('.fa-solid');
+
+    if(menuMobile.classList.contains('open')) {
+        menuMobile.classList.remove('open');
+        icone.classList.remove('fa-xmark')
+        icone.classList.add('fa-bars')
+        
+    } else {
+        menuMobile.classList.add('open')
+        icone.classList.remove('fa-bars')
+        icone.classList.add('fa-xmark')
+    }
+    
+}
+
+
+
+
 const prev = document.getElementById('prev-bt');
 const next = document.getElementById('next-bt');
 const slides = document.querySelectorAll('.slider')
@@ -33,6 +53,8 @@ function prevSlide(){
 
 prev.addEventListener('click',nextSlide)
 next.addEventListener('click',prevSlide)
+
+const sliderAut = setInterval(nextSlide, 4000)
 
 
 console.log('olá')
