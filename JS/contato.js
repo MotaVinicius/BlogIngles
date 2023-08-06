0f9818881de4c344e1e20284accb5e95dba440cf
Vinicius Mota <motavinicius@outlook.com>
2023-08-06 16:34:35 -0300

    menu mobile

--- JS/contato.js
+++ JS/contato.js
@@ -89,3 +89,22 @@ function enviar() {
     let popUp = document.querySelector('#popUpWindow')
     popUp.style.display = 'none'
  }
+
+ /*menu mobile*/
+
+function menuShow(){
+    let menuMobile = document.querySelector('.menu-mobile');
+    let icone = document.querySelector('.fa-solid');
+
+    if(menuMobile.classList.contains('open')) {
+        menuMobile.classList.remove('open');
+        icone.classList.remove('fa-xmark')
+        icone.classList.add('fa-bars')
+        
+    } else {
+        menuMobile.classList.add('open')
+        icone.classList.remove('fa-bars')
+        icone.classList.add('fa-xmark')
+    }
+    
+}
--- JS/scriptPhrasalVerbs.js
+++ JS/scriptPhrasalVerbs.js
@@ -1,3 +1,5 @@
+/*tabela*/
+
 const fieldSearch = document.getElementById('txt-busca');
 const conteudo = document.getElementById('dados');
 
@@ -21,3 +23,22 @@ fieldSearch.addEventListener('keyup', () => {
     }
 
 }});
+
+/*menu mobile*/
+
+function menuShow(){
+    let menuMobile = document.querySelector('.menu-mobile');
+    let icone = document.querySelector('.fa-solid');
+
+    if(menuMobile.classList.contains('open')) {
+        menuMobile.classList.remove('open');
+        icone.classList.remove('fa-xmark')
+        icone.classList.add('fa-bars')
+        
+    } else {
+        menuMobile.classList.add('open')
+        icone.classList.remove('fa-bars')
+        icone.classList.add('fa-xmark')
+    }
+    
+}
